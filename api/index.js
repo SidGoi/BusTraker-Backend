a2fafd83e2e5c7c73c54c3cc42e2e126bb59fc98
siddharaj <gohilsiddharaj13325@gmail.com>
2026-01-30 00:09:51 +0530

    done

--- api/index.js
+++
@@ -1,6 +0,0 @@
-import app from "../src/app.js"; // path to your current app.js
-import connectDB from "../src/config/db.js"; // your MongoDB connection
-
-connectDB(); // connect to MongoDB Atlas
-
-export default app;
